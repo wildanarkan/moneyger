220c304652a33d044a410333dd97096b069da1c5
Ahmad Wildan Arkan <cillmystic@gmail.com>
2025-06-13 14:37:46 +0700

    refactor: icon android app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:moneyger_3_24/main.dart';
+import 'package:moneyger/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
